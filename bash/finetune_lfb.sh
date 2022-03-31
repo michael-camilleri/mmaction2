ee00c5b07aceb2f7b6c0eea4c4efd650797e5de4
Michael P Camilleri <michael.p.camilleri@ed.ac.uk>
2022-03-31 16:56:05 +0100

    Scripts
    Scripts
     * Updated Finetuning script to be more robust to changes in parameters

--- bash/finetune_lfb.sh
+++ bash/finetune_lfb.sh
@@ -10,6 +10,7 @@
 #     [Images]   - Number of Images (Samples) per-GPU
 #     [Rate]     - Learning Rate (actual value used, not dependent on Batch-Size)
 #     [Epochs]   - Maximum Number of Epochs to train for
+#     [Warmup]   - Warmup Period (epochs)
 #     [Offset]   - Offset from base data location to retrieve the data splits
 #     [Frames]   - Y/N: Indicates if Frames should be rsynced: this is done to save time if it is
 #                       known that the machine contains the right data splits.
@@ -24,14 +25,32 @@
 #        in my Jupyter notebook.
 #    Model PTHs are under ${HOME}/models/LFB/Base/ : Configs are part of the Repository
 
-# Do some Calculations/Preprocessing
-BATCH_SIZE=$(echo "${1} * ${2}" | bc)
-OUT_NAME=${4}_${BATCH_SIZE}_${3}_S10
+####  Some Configurations
+# Get and store the main Parameters
+GPU_NODES=${1}
+IMAGE_GPU=${2}
+LEARN_RATE=${3}
+MAX_EPOCHS=${4}
+WARMUP_ITER=${5}
+PATH_OFFSET=${6}
+FORCE_FRAMES=${7,,}
+FORCE_LFB=${8,,}
+
+# Derivative Values
+BATCH_SIZE=$(echo "${GPU_NODES} * ${IMAGE_GPU}" | bc)
+OUT_NAME=${MAX_EPOCHS}_${BATCH_SIZE}_L${LEARN_RATE}_W${WARMUP_ITER}
+
+# Path Values
+SCRATCH_HOME=/disk/scratch/${USER}
+SCRATCH_DATA=${SCRATCH_HOME}/data/behaviour
+SCRATCH_MODELS=${SCRATCH_HOME}/models/lfb_train
+SCRATCH_OUT=${SCRATCH_HOME}/results_train
+OUTPUT_DIR="${HOME}/models/LFB/Trained/${PATH_OFFSET}"
 
 # ===================
 # Environment setup
 # ===================
-echo "Setting up Conda enviroment on ${SLURM_JOB_NODELIST} with ${1} GPU(s): Config=${OUT_NAME}"
+echo "Setting up Conda enviroment on ${SLURM_JOB_NODELIST} with ${GPU_NODES} GPU(s): Config=${OUT_NAME}"
 set -e # Make script bail out after first error
 source activate py3mma   # Activate Conda Environment
 echo "Libraries from: ${LD_LIBRARY_PATH}"
@@ -40,7 +59,6 @@ echo "Libraries from: ${LD_LIBRARY_PATH}"
 export NCCL_DEBUG=INFO
 
 # Make your own folder on the node's scratch disk
-SCRATCH_HOME=/disk/scratch/${USER}
 mkdir -p ${SCRATCH_HOME}
 echo ""
 
@@ -49,25 +67,24 @@ echo ""
 # ================================
 echo " ===================================="
 echo "Consolidating Data/Models in ${SCRATCH_HOME}"
-SCRATCH_DATA=${SCRATCH_HOME}/data/behaviour/
 mkdir -p ${SCRATCH_DATA}
 echo "  -> Synchronising Data"
 echo "     .. Schemas .."
 cp ${HOME}/data/behaviour/Common/AVA* ${SCRATCH_DATA}/
 echo "     .. Annotations .."
 rsync --archive --update --compress --include '*/' --include 'AVA*' --exclude '*' \
-      --info=progress2 ${HOME}/data/behaviour/Train/${5}/ ${SCRATCH_DATA}
-if [ "${6,,}" = "y" ]; then
+      --info=progress2 ${HOME}/data/behaviour/Train/${PATH_OFFSET}/ ${SCRATCH_DATA}/
+if [ "${FORCE_FRAMES}" = "y" ]; then
   echo "     .. Frames .."
-  rsync --archive --update --info=progress2 ${HOME}/data/behaviour/Train/Frames ${SCRATCH_DATA}
+  rsync --archive --update --info=progress2 ${HOME}/data/behaviour/Train/Frames ${SCRATCH_DATA}/
 else
   echo "     .. Skipping Frames .."
 fi
 echo " ------------------------------"
 echo "  -> Synchronising Models"
-SCRATCH_MODELS=${SCRATCH_HOME}/models/lfb_train
 echo "   .. Copying Models .. "
 mkdir -p ${SCRATCH_MODELS}
+mkdir -p ${SCRATCH_OUT}
 rsync --archive --update --compress ${HOME}/models/LFB/Base/ ${SCRATCH_MODELS}/
 echo "   .. Synchronising and Formatting Configs .. "
 cp ${HOME}/code/MMAction/configs/own/backbone.base.py ${SCRATCH_MODELS}/backbone.base.py
@@ -86,8 +103,7 @@ cp ${HOME}/code/MMAction/configs/own/train_sgd.base.py ${SCRATCH_MODELS}/train.p
 sed -i "s@<SOURCE>@${SCRATCH_DATA}@" ${SCRATCH_MODELS}/train.py
 sed -i "s@<FEATUREBANK>@${SCRATCH_DATA}/feature_bank@" ${SCRATCH_MODELS}/train.py
 sed -i "s@<MODELINIT>@${SCRATCH_MODELS}/inference.base.pth@" ${SCRATCH_MODELS}/train.py
-sed -i "s@<MODELOUT>@${SCRATCH_MODELS}/out@" ${SCRATCH_MODELS}/train.py
-mkdir -p ${SCRATCH_MODELS}/out
+sed -i "s@<MODELOUT>@${SCRATCH_OUT}@" ${SCRATCH_MODELS}/train.py
 echo "    == Models Done =="
 mail -s "Train_LFB on ${SLURM_JOB_NODELIST}:${OUT_NAME}" ${USER}@sms.ed.ac.uk <<< "Synchronised Data and Models."
 echo ""
@@ -99,7 +115,7 @@ echo ""
 echo " ===================================="
 echo " Generating Feature-Bank Vectors "
 echo "  -> Training Set"
-if [ -f "${SCRATCH_DATA}/feature_bank/lfb_Train.pkl" ] && [ "${7,,}" = "n" ]; then
+if [ -f "${SCRATCH_DATA}/feature_bank/lfb_Train.pkl" ] && [ "${FORCE_LFB}" = "n" ]; then
   echo "    Training FB Exists and not Forced to regenerate: skipping."
 else
   echo "    Re-Generating"
@@ -111,7 +127,7 @@ else
 fi
 echo " ------------------------------"
 echo "  -> Validation Set"
-if [ -f "${SCRATCH_DATA}/feature_bank/lfb_Validate.pkl" ] && [ "${7,,}" = "n" ]; then
+if [ -f "${SCRATCH_DATA}/feature_bank/lfb_Validate.pkl" ] && [ "${FORCE_LFB}" = "n" ]; then
   echo "    Validation FB Exists and not Forced to regenerate: skipping."
 else
   echo "    Re-Generating"
@@ -133,11 +149,11 @@ echo ""
 # Train Model
 # ===========
 echo " ===================================="
-echo " Training Model with ${1} GPU(s)  (BS=${BATCH_SIZE}, LR=${3}) for ${4} epochs"
-python -m torch.distributed.launch --nproc_per_node=${1} tools/train.py \
+echo " Training Model with ${GPU_NODES} GPU(s)  (BS=${BATCH_SIZE}, LR=${LEARN_RATE}) for ${MAX_EPOCHS} epochs"
+python -m torch.distributed.launch --nproc_per_node=${GPU_NODES} tools/train.py \
     ${SCRATCH_MODELS}/train.py --launcher pytorch \
     --validate --seed 0 --deterministic \
-    --cfg-options data.videos_per_gpu=${2} optimizer.lr=${3} total_epochs=${4}
+    --cfg-options data.videos_per_gpu=${IMAGE_GPU} optimizer.lr=${LEARN_RATE} total_epochs=${MAX_EPOCHS} lr_config.warmup_iters=${WARMUP_ITER}
 echo "   == Training Done =="
 mail -s "Train_LFB on ${SLURM_JOB_NODELIST}:${OUT_NAME}" ${USER}@sms.ed.ac.uk <<< "Model Training Completed."
 echo ""
@@ -146,7 +162,6 @@ echo ""
 # Copy Data
 # ===========
 echo " ===================================="
-OUTPUT_DIR="${HOME}/models/LFB/Trained/${5}"
 mkdir -p ${OUTPUT_DIR}
 echo " Copying Model Weights to ${OUTPUT_DIR}/${OUT_NAME}"
 rsync --archive --compress --info=progress2 "${SCRATCH_MODELS}/out/" "${OUTPUT_DIR}/${OUT_NAME}"
